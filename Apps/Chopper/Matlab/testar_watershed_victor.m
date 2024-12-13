ff311354aeb9b9296eb93b39bc7d72e7e171a7f8
guiguilhermegui <39204794+guiguilhermegui@users.noreply.github.com>
2024-12-13 13:00:14 -0300

    Update and rename region_watershed.m to testar_watershed_victor.m

--- Apps/Chopper/Matlab/testar_watershed_victor.m
+++ Apps/Chopper/Matlab/testar_watershed_victor.m
@@ -1,3 +1,20 @@
+% copiar e colar código https://www.mathworks.com/help/images/marker-controlled-watershed-segmentation.html
+%testar até funfar
+% unificar com o Chopper
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function segmented_image = region_segmentation(image)
 
     % Convert to double precision for numerical operations
